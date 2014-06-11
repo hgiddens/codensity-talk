1948a6341c88870093d98e0d3972e105a49d5253
Hugh Giddens <hgiddens@atlassian.com>
2014-06-11 17:27:17 +1000

    It'd shit me to lose all this now.

--- Main.scala
+++ Main.scala
@@ -2,8 +2,7 @@ import org.scalameter.api._
 import scalaz._, Scalaz._
 
 trait TreeLike[F[_]] {
-  // XXX can ditch call by name?
-  def node[A](l: ⇒ F[A], r: ⇒ F[A]): F[A]
+  def node[A](l: => F[A], r: => F[A]): F[A]
 }
 
 object Strict {
@@ -13,82 +12,89 @@ object Strict {
 
   object Tree {
     implicit object treeMonad extends Monad[Tree] {
-      def point[A](a: ⇒ A): Tree[A] = Leaf(a)
-      def bind[A, B](fa: Tree[A])(f: A ⇒ Tree[B]) = fa match {
-        case Leaf(a) ⇒ f(a)
-        case Node(l, r) ⇒ Node(bind(l)(f), bind(r)(f))
+      def point[A](a: => A): Tree[A] = Leaf(a)
+      def bind[A, B](fa: Tree[A])(f: A => Tree[B]) = fa match {
+        case Leaf(a) => f(a)
+        case Node(l, r) => Node(bind(l)(f), bind(r)(f))
       }
     }
   }
 
   def fullTree(i: Int): Tree[Int] = i match {
-    case 1 ⇒ Leaf(1)
-    case n ⇒
+    case 1 => Leaf(1)
+    case n =>
       val j = n - 1
       for {
-        t ← fullTree(j)
-        node ← Node(Leaf(j - 1), Leaf(j + 1)): Tree[Int]
+        t <- fullTree(j)
+        node <- Node(Leaf(j - t), Leaf(t + 1)): Tree[Int]
       } yield node
   }
 
   def zigzag(tree: Tree[Int]): Int = {
     def zig(t: Tree[Int]): Int = t match {
-      case Leaf(n) ⇒ n
-      case n: Node[Int] ⇒ zag(n.l)
+      case Leaf(n) => n
+      case n: Node[Int] => zag(n.l)
     }
     def zag(t: Tree[Int]): Int = t match {
-      case Leaf(n) ⇒ n
-      case n: Node[Int] ⇒ zig(n.r)
+      case Leaf(n) => n
+      case n: Node[Int] => zig(n.r)
     }
     zig(tree)
   }
 }
 
 object Lazy {
+  var leaves = 0
+  var nodes = 0
+  def tell = (nodes, leaves, nodes + leaves)
+  def reset() = { leaves = 0; nodes = 0 }
   sealed trait Tree[A]
-  case class Leaf[A](a: A) extends Tree[A]
-  class Node[A](_l: ⇒ Tree[A], _r: ⇒ Tree[A]) extends Tree[A] {
+  case class Leaf[A](a: A) extends Tree[A] {
+    leaves += 1
+  }
+  class Node[A](_l: => Tree[A], _r: => Tree[A]) extends Tree[A] {
+    nodes += 1
     lazy val l = _l
     lazy val r = _r
   }
 
   object Tree {
     implicit object treeMonad extends Monad[Tree] {
-      def point[A](a: ⇒ A): Tree[A] = Leaf(a)
-      def bind[A, B](fa: Tree[A])(f: A ⇒ Tree[B]) = fa match {
-        case Leaf(a) ⇒ f(a)
-        case n: Node[A] ⇒ new Node(bind(n.l)(f), bind(n.r)(f))
+      def point[A](a: => A): Tree[A] = Leaf(a)
+      def bind[A, B](fa: Tree[A])(f: A => Tree[B]) = fa match {
+        case Leaf(a) => f(a)
+        case n: Node[A] => new Node(bind(n.l)(f), bind(n.r)(f))
       }
     }
 
     implicit object treeTreeLike extends TreeLike[Tree] {
-      def node[A](l: ⇒ Tree[A], r: ⇒ Tree[A]): Tree[A] =
+      def node[A](l: => Tree[A], r: => Tree[A]): Tree[A] =
         new Node(l, r)
     }
   }
 
-  def fullTree[F[_]: Monad](i: Int)(implicit F: TreeLike[F]): F[Int] =
+  def fullTree[F[_]: Monad](i: Int)(implicit FT: TreeLike[F]): F[Int] =
     i match {
-      case 1 ⇒ 1.point[F]
-      case n ⇒
+      case 1 => 1.point[F]
+      case n =>
         val j = n - 1
-        for {
-          t ← fullTree[F](j)
-          //l = (j - 1).point[F]
-          //r = (j + 1).point[F]
-          //node ← F.node(l, r)
-          node ← F.node((j - 1).point[F], (j + 1).point[F])
-        } yield node
+        fullTree[F](j).flatMap { t =>
+          FT.node((j - t).point[F], (t + 1).point[F])
+        }
+        // for {
+        //   t <- fullTree[F](j)
+        //   node <- FT.node((j - 1).point[F], (j + 1).point[F])
+        // } yield node
     }
 
   def zigzag(tree: Tree[Int]): Int = {
     def zig(t: Tree[Int]): Int = t match {
-      case Leaf(n) ⇒ n
-      case n: Node[Int] ⇒ zag(n.l)
+      case Leaf(n) => n
+      case n: Node[Int] => zag(n.l)
     }
     def zag(t: Tree[Int]): Int = t match {
-      case Leaf(n) ⇒ n
-      case n: Node[Int] ⇒ zig(n.r)
+      case Leaf(n) => n
+      case n: Node[Int] => zig(n.r)
     }
     zig(tree)
   }
@@ -98,9 +104,9 @@ object CodensityStuff {
   import Lazy._
   type CodensityTree[A] = Codensity[Tree, A]
   implicit object codensityTreeTreeLike extends TreeLike[CodensityTree] {
-    def node[A](l: ⇒ CodensityTree[A], r: ⇒ CodensityTree[A]): CodensityTree[A] =
+    def node[A](l: => CodensityTree[A], r: => CodensityTree[A]): CodensityTree[A] =
       new Codensity[Tree, A] {
-        def apply[B](f: A ⇒ Tree[B]) =
+        def apply[B](f: A => Tree[B]) =
           implicitly[TreeLike[Tree]].node(l.apply(f), r.apply(f))
       }
   }
@@ -118,21 +124,21 @@ object TreeBenchmark extends PerformanceTest {
 
   performance of "fullTree" in {
     measure method "strict" in {
-      val strictTreeSizes = Gen.range("strict size")(2, 20, 1)
-      using(strictTreeSizes) in { n ⇒
+      val strictTreeSizes = Gen.range("strict size")(2, 21, 1)
+      using(strictTreeSizes) in { n =>
         import Strict._
         val tree = fullTree(n)
         zigzag(tree)
       }
 
       val lazyTreeSizes = Gen.range("lazy size")(2, 2000, 10)
-      using(lazyTreeSizes) in { n ⇒
+      using(lazyTreeSizes) in { n =>
         import Lazy._
         val tree = fullTree[Tree](n)
         zigzag(tree)
       }
 
-      using(lazyTreeSizes) in { n ⇒
+      using(lazyTreeSizes) in { n =>
         import CodensityStuff._
         import Lazy._
         val tree = fullTree[CodensityTree](n)
