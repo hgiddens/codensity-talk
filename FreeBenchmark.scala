999039c6905d80a8f8f39b07584b1d0361571e67
Hugh Giddens <hgiddens@atlassian.com>
2014-06-25 17:03:45 +1000

    Church-encoded Free monad.

--- FreeBenchmark.scala
+++ FreeBenchmark.scala
@@ -2,6 +2,10 @@ import language._
 import org.scalameter.api._
 import scalaz._, Scalaz._
 
+import concurrent._
+import duration._
+import ExecutionContext.Implicits._
+
 trait FreeLike[M[_[_], _]] {
   implicit def monad[F[_]: Functor]: Monad[({ type l[a] = M[F, a] })#l]
   def liftFree[F[_]: Functor, A](fa: F[A]): M[F, A]
@@ -69,6 +73,53 @@ object SFree {
   }
 }
 
+object Ch {
+  type Rec[F[_], R] = (F[R] => R) => R
+  type YF[F[_], A] = Yoneda[({ type l[a] = Rec[F, a] })#l, A]
+
+  def yfFunctor[F[_]]: Functor[({ type l[a] = YF[F, a] })#l] =
+    Yoneda.yonedaFunctor[({ type l[a] = Rec[F, a] })#l]
+
+  implicit def yfMonad[F[_]]: Monad[({ type l[a] = YF[F, a] })#l] =
+    new Monad[({ type l[a] = YF[F, a] })#l] {
+      type RecF[A] = Rec[F, A]
+      def point[A](a: => A): Yoneda[RecF, A] =
+        new Yoneda[RecF, A] {
+          def apply[B](f: A => B): RecF[B] =
+            _ => f(a)
+        }
+
+      override def map[A,B](fa: Yoneda[RecF, A])(f: A => B): Yoneda[RecF, B] =
+        new Yoneda[RecF,B] {
+          def apply[C](kp: B => C): RecF[C] =
+            fa(kp compose f)
+        }
+
+      def bind[A,B](fa: Yoneda[RecF, A])(f: A => Yoneda[RecF, B]): Yoneda[RecF, B] =
+        new Yoneda[RecF, B] {
+          def apply[C](kp: B => C): RecF[C] = { kf: (F[C] => C) =>
+            fa((a: A) => f(a)(kp)(kf))(kf)
+          }
+        }
+    }
+
+  implicit def yfFreeLike: FreeLike[YF] =
+    new FreeLike[YF] {
+      implicit def monad[F[_]: Functor]: Monad[({ type l[a] = YF[F, a] })#l] = yfMonad[F]
+
+      def liftFree[F[_], A](fa: F[A])(implicit F: Functor[F]): YF[F, A] =
+        new YF[F, A] {
+          def apply[B](kp: A => B): (F[B] => B) => B = { kf: (F[B] => B) =>
+            kf(F.map(fa)(kp))
+          }
+        }
+    }
+
+  def retract[F[_], A](yffa: YF[F, A])(implicit F: Monad[F]): F[A] =
+    yffa(F.point[A](_))(F.join[A](_))
+}
+
+
 object Stuffs {
   type Username = String
   case class User(name: Username)
@@ -76,6 +127,9 @@ object Stuffs {
     List.range(0, n).map(i => s"user-$i")
   def user[M[_]: Monad](username: Username): M[User] =
     User(username).point[M]
+
+  def go[M[_]](n: Int)(implicit M: Monad[M]): M[List[User]] =
+    users(n).traverseU(username => user[M](username))
 
   def gof[F[_[_],_], M[_]](n: Int)(implicit F: FreeLike[F], M: Monad[M]): F[M, List[User]] = {
     import F.{ monad, syntax }
@@ -93,24 +147,48 @@ object FreeBenchmark extends PerformanceTest {
 
   performance of "stuff" in {
     measure method "misc" in {
-      val sizes = Gen.range("sizes")(0, 1000, 25)
+      val sizes = Gen.range("sizes")(0, 30000, 5000)
       val flags = (
         exec.jvmflags -> "-Xmx4g -Xms4g -XX:CompileThreshold=100 -Xss64m"
       )
       import NaturalTransformation._
 
-      using(sizes) config flags in { n =>
-        Stuffs.gof[SFree, Id](n).foldMap(id).copoint
-      }
+      //using(sizes) /*config flags*/ in { n =>
+      //  Stuffs.gof[SFree, Id](n).foldMap(id).copoint
+      //}
       
-      using(sizes) config flags in { n =>
-        type CFLSFree[F[_], A] = FreeLike.CFL[SFree, F, A]
-        Stuffs.gof[CFLSFree, Id](n).improve.foldMap(id).copoint
-      }
+      //using(sizes) config flags in { n =>
+      //  type CFLSFree[F[_], A] = FreeLike.CFL[SFree, F, A]
+      //  Stuffs.gof[CFLSFree, Id](n).improve.foldMap(id).copoint
+      //}
+
+      //using(sizes) /*config flags*/ in { n =>
+      //  Stuffs.gof[Free, Id](n).foldMap(id).copoint
+      //}
 
       using(sizes) config flags in { n =>
-        Stuffs.gof[Free, Id](n).foldMap(id).copoint
+        import Ch._
+        retract(Stuffs.gof[YF, Id](n)).copoint
       }
+
+      //using(sizes) /* config flags */ in { n =>
+      //  type A[X] = Id[X]
+      //  type B[X] = ReaderT[A, Unit, X]
+      //  type C[X] = WriterT[B, Unit, X]
+      //  type D[X] = Free[C, X]
+      //
+      //  val (a,b) = Stuffs.go[D](n).foldMap(refl).run(()).copoint
+      //}
+      //
+      //using(sizes) in { n =>
+      //  import Ch._
+      //  type A[X] = Id[X]
+      //  type B[X] = ReaderT[A, Unit, X]
+      //  type C[X] = WriterT[B, Unit, X]
+      //  type D[X] = YF[C, X]
+      //
+      //  val (a,b) = retract(Stuffs.go[D](n)).run(()).copoint
+      //}
     }
   }
 }
